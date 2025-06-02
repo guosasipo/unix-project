83cd06a8d4b637b0c87d7b64b877d7b3834b5d1b
Lee HoJoon <leehojoonlee0820@gmail.com>
2025-06-03 04:02:29 +0900

    Update user_log_tool.sh

--- user_log_tool.sh
+++ user_log_tool.sh
@@ -37,7 +37,8 @@ user_commands() {
     {
         echo "*** 최근 20개 사용자 명령어 기록 ***"
         echo ""
-        history | tail -n 20 | sed 's/^[ ]*[0-9]*[ ]*//'
+        HIST=$(history)
+        echo "$HIST" | tail -n 20 | sed 's/^[ ]*[0-9]*[ ]*//'
     } > user_commands.txt
     echo -e "${GREEN_BG}${BOLD} 최근 20개 명령어가 user_commands.txt에 저장되었습니다. ${NC}"
 }
